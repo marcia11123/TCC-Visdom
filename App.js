56992c691b809cae9383bc6a39c99fcc277bd882
marcia <marciavitoria.felix.9@gmail.com>
2024-03-05 14:38:54 -0300

    Criando projeto

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+      <text style={styles.textHeader}>Visdom</text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +17,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  header:{
+    justifyContent: 'top',
+    width:"100%",
+    height:"10%",
+    backgroundColor:'#000',
+  },
+  textHeader:{
+    fontSize:20,
+    color:'#fff'
+  }
 });
