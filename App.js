6960c3698a57a828deeffb0817101e22b6ba6db4
marcia <marciavitoria.felix.9@gmail.com>
2024-05-17 11:55:38 -0300

    atualização 17-05-24

--- App.js
+++ App.js
@@ -1,33 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
+import Tela1 from './components/Tela1.js';
+import Tela2 from './components/Tela2.js';
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-      <text style={styles.textHeader}>Visdom</text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator
+        screenOptions={{
+          tabBarStyle:{
+            backgroundColor:'#181b1cbd',
+            height: 60
+          },
+          tabBarLabelStyle:{
+            fontSize: 22,
+            fontWeight: '500',
+            color: '#4babe5'
+          },
+          tabBarActiveTintColor:'#009af7'
+        }}
+      >
+        <Tabs.Screen
+          name='Tela 1'
+          component={Tela1}
+          options={{
+            tabBarIcon: ({color, size})=>(<FontAwesome name="th-large" color={color} size={size} />)
+          }}
+        />
+        <Tabs.Screen
+          name='Tela 2'
+          component={Tela2}
+        />
+        
+        
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    
-  },
-  header:{
-    justifyContent: 'top',
-    width:"100%",
-    height:"10%",
-    backgroundColor:'#000',
-  },
-  textHeader:{
-    fontSize:20,
-    color:'#fff',
-   
-  }
-});
